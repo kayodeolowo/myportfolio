b731fed34f20b94e12217b37a3f4c50f2a3a4d4f
kayodeolowo <officialkayodeolowo@gmail.com>
2023-06-19 14:23:39 +0100

    data

--- src/Data.js
+++ src/Data.js
@@ -39,17 +39,17 @@ const Categories = [
 
     },
 
-       {
-       id:12,
-         categories: "websites",
-        name: "Spowse",
-        img : "./images/spowse.png",
-       description: "Launching soon webiste for a social media Application, ",
-        languages: ["Javascript,", "ReactJS,", "HTML5,", "CSS3"],
-        link: "https://spwose.netlify.app/",
-        github: "https://github.com/kayodeolowo"
+    //    {
+    //    id:12,
+    //      categories: "websites",
+    //     name: "Spowse",
+    //     img : "./images/spowse.png",
+    //    description: "Launching soon webiste for a social media Application, ",
+    //     languages: ["Javascript,", "ReactJS,", "HTML5,", "CSS3"],
+    //     link: "https://spwose.netlify.app/",
+    //     github: "https://github.com/kayodeolowo"
 
-    },
+    // },
 
 
 
