d4d10f9cf87635c5364661d44232d96dde479e72
kayodeolowo <officialkayodeolowo@gmail.com>
2023-11-14 04:51:17 +0100

    data

--- src/Data.js
+++ src/Data.js
@@ -2,227 +2,229 @@ const Categories = [
 
 
   {
-    id:15,
-      categories: "web apps",
-     name: "Moon Republic",
-     img : "./images/moonrep.jpg",
-     languages: ["Javascript", "ReactJS"],
-     link: "https://moonrepublic.io/",
-     github: "https://github.com/kayodeolowo",
-     data: "Json"
+    id: 15,
+    categories: "web apps",
+    name: "Moon Republic",
+    img: "./images/moonrep.jpg",
+    languages: ["Javascript", "ReactJS"],
+    link: "https://moonrepublic.io/",
+    github: "https://github.com/kayodeolowo",
+    data: "Json"
 
- },
-
-
-
-    {
-      
-       id:13,
-         categories: "web apps",
-        name: "KrayaStores",
-        img : "./images/ecom.png",
-       description: "An E-commerce website for shopping ",
-        languages: ["Typescript", "ReactJS", "Redux"],
-        link: "https://krayastores.netlify.app/",
-        github: "https://github.com/kayodeolowo/e-commerce",
-        data: "Rest Api"
-
-    },
-
-    {
-      id:14,
-        categories: "web apps",
-       name: "Marketie",
-       img : "./images/dashboard.jpg",
-      description: "Sales dashboard that provides real-time details on sales performance, Earnings, top-selling products and others ",
-       languages: ["Javascript", "ReactJS"],
-       link: "https://sales-dahsboard.netlify.app/dashboard",
-       github: "https://github.com/kayodeolowo",
-       data: "Json"
-
-   },
-
-   {
-    id:16,
-      categories: "web apps",
-     name: "  Begreat Finance",
-     img : "./images/begreat.jpg",
-     languages: ["Javascript", "Next JS", "Redux"],
-     link: "https://begreat.finance/",
-     github: "https://github.com/kayodeolowo",
-     data: "Json"
-  
   },
 
-   
+
+
   {
-    id:2,
-      categories: "web apps",
-     name: "Movie Trailer",
-     img : "./images/movie.png",
+
+    id: 13,
+    categories: "web apps",
+    name: "KrayaStores",
+    img: "./images/ecom.png",
+    description: "An E-commerce website for shopping ",
+    languages: ["Typescript", "ReactJS", "Redux"],
+    link: "https://krayastores.netlify.app/",
+    github: "https://github.com/kayodeolowo/e-commerce",
+    data: "Rest Api"
+
+  },
+
+  {
+    id: 14,
+    categories: "web apps",
+    name: "Marketie",
+    img: "./images/dashboard.jpg",
+    description: "Sales dashboard that provides real-time details on sales performance, Earnings, top-selling products and others ",
+    languages: ["Javascript", "ReactJS"],
+    link: "https://sales-dahsboard.netlify.app/dashboard",
+    github: "https://github.com/kayodeolowo",
+    data: "Json"
+
+  },
+
+  {
+    id: 16,
+    categories: "web apps",
+    name: "  Begreat Finance",
+    img: "./images/begreat.jpg",
+    languages: ["Javascript", "Next JS", "Redux"],
+    link: "https://begreat.finance/",
+    github: "https://github.com/kayodeolowo",
+    data: "Json"
+
+  },
+
+
+  {
+    id: 2,
+    categories: "web apps",
+    name: "Movie Trailer",
+    img: "./images/movie.png",
     description: "A Web App that displays movies and tv shows with their trailers ",
-     languages: ["Javascript", "ReactJS" , "API"],
-     link: "https://k-streams.netlify.app/",
-     github: "https://github.com/kayodeolowo/movies-stream",
-     data: "Rest Api"
+    languages: ["Javascript", "ReactJS", "API"],
+    link: "https://k-streams.netlify.app/",
+    github: "https://github.com/kayodeolowo/movies-stream",
+    data: "Rest Api"
 
- },
+  },
 
-    {
-       id:4,
-         categories: "websites",
-        name: "Ms lockdown",
-        img : "./images/mslockdown.png",
-       description: "Website built for a social media Startup ",
-        languages: ["Javascript", "ReactJS"],
-        link: "https://mslockdown.netlify.app/",
-        github: "https://github.com/kayodeolowo"
+  {
+    id: 4,
+    categories: "websites",
+    name: "Ms lockdown",
+    img: "./images/mslockdown.png",
+    description: "Website built for a social media Startup ",
+    languages: ["Javascript", "ReactJS"],
+    link: "https://mslockdown.netlify.app/",
+    github: "https://github.com/kayodeolowo"
 
-    },
+  },
 
-     {
-       id:1,
-         categories: "web apps",
-        name: "Crypto Stats",
-        img : "./images/crypto.png",
-       description: " Web App were users can check different Live crypto currencies with their individual data ",
-        languages: ["Javascript", "ReactJS",  "Firebase", "API"],
-        link: "https://mycrypto-stats.netlify.app/",
-        github: "https://github.com/kayodeolowo/mycryptoStats",
-        data: "Rest Api"
+  {
+    id: 1,
+    categories: "web apps",
+    name: "Crypto Stats",
+    img: "./images/crypto.png",
+    description: " Web App were users can check different Live crypto currencies with their individual data ",
+    languages: ["Javascript", "ReactJS", "Firebase", "API"],
+    link: "https://mycrypto-stats.netlify.app/",
+    github: "https://github.com/kayodeolowo/mycryptoStats",
+    data: "Rest Api"
 
-    },
+  },
 
-       {
-       id:12,
-         categories: "websites",
-        name: "ArbFurbo",
-        img : "./images/arb.jpg",
-       description: "Web 3 web app that operates on top of a decentralized network and utilizes blockchain technology",
-        languages: ["Javascript", "ReactJS", ],
-        link: "https://www.arbfurbo.wtf/",
-        github: "https://github.com/kayodeolowo"
+  {
+    id: 12,
+    categories: "websites",
+    name: "ArbFurbo",
+    img: "./images/arb.jpg",
+    description: "Web 3 web app that operates on top of a decentralized network and utilizes blockchain technology",
+    languages: ["Javascript", "ReactJS",],
+    link: "https://www.arbfurbo.wtf/",
+    github: "https://github.com/kayodeolowo"
 
-    },
-
-
+  },
 
 
-       {
-       id:3,
-         categories: "websites",
-        name: "Edushare",
-        img : "./images/edushare.png",
-       description: "An Educational landing page for a tuition-free online public school ",
-        languages: ["Javascript", "ReactJS", ],
-        link: "https://myedushare.netlify.app/",
-        github: "https://github.com/kayodeolowo/edushare-Landing-page"
+  {
+    id: 7,
+    categories: "web apps",
+    name: "Country Factzone",
+    type: "Rest Api",
+    img: "./images/country.png",
+    description: "This site displays all continents, countries and their details from an api. ",
+    languages: ["Javascript", "ReactJS", "API"],
+    link: "https://countries-facts.netlify.app/",
+    github: "https://github.com/kayodeolowo/Country-factsZone",
+    data: "Rest Api"
 
-    },
-
-
- 
-
-    {
-       id:5,
-         categories: "websites",
-        name: "Tasty Foods",
-        img : "./images/Tasty.png",
-       description: "A Mobile Responsive Landing page for an Eatery  ",
-        languages: ["Javascript", "ReactJS", ],
-        link: "https://tasty-foodies.netlify.app/",
-        github: "https://github.com/kayodeolowo/foodweb"
-
-    },
-
-     {
-       id:6,
-         categories: "web apps",
-        name: "Budget Calculator",
-        img : "./images/budget.png",
-       description: "A website for totaling your daily expenditures. ",
-        languages: ["Javascript", "ReactJS", ],
-        link: "https://mybudgets-calculator.netlify.app/",
-        github: "https://github.com/kayodeolowo/expenses_Calculator",
-        data: "JSon"
-
-    },
-
-   
-    {
-        id:7,
-         categories: "web apps",
-        name: "Country Factzone",
-        type: "Rest Api",
-        img : "./images/country.png",
-        description: "This site displays all continents, countries and their details from an api. ",
-         languages: ["Javascript", "ReactJS", "API" ],
-        link: "https://countries-facts.netlify.app/",
-        github: "https://github.com/kayodeolowo/Country-factsZone",
-        data: "Rest Api"
-
-    },
-
-    {
-        id:8,
-         categories: "websites",
-        name: "Space Tourism",
-        type: "JSON",
-        img : "./images/spacetour.png",
-        description: "Space Tourism website with details about space Trips. ",
-         languages: ["Javascript","ReactJS", "Json" ],
-        link: "https://our-space-launch.netlify.app",
-        github: "https://github.com/kayodeolowo/Space-Tour",
-        data: "JSon"
-
-    },
-
-     {
-       id:9,
-         categories: "web apps",
-        name: "My Portfolio",
-         type: "Json",
-        img : "./images/portf.png",
-       description: "Hi Welcome to my portfolio. ",
-        languages: ["Javascript", "ReactJS", "Json" ],
-        link: "https://kayodeolowo.netlify.app/",
-        github: "https://github.com/kayodeolowo/myportfolio",
-        data: "JSon"
-
-    },
+  },
 
 
-    {
-       id:10,
-         categories: "websites",
-        name: "Eazy Bank",
-        img : "./images/eazybank.png",
-        description: "A responsive Banking landing page. ",
-         languages: ["Javascript", "ReactJS", ],
-        link: "https://my-eazy-bank.netlify.app/",
-        github: "https://github.com/kayodeolowo/easebank"
 
-    },
 
-    // {
-    //    id:11,
-    //      categories: "web apps",
-    //     name: "Currency Converter",
-    //     type: "Rest API",
-    //     img : "./images/converter.png",
-    //    description: "Web App built with the functionality of Accurately converting currencies. ",
-    //     languages: ["Javascript", "ReactJS", "Api" ],
-        
-    //     link: "https://my-currencies-converter.netlify.app/",
-    //     github: "https://github.com/kayodeolowo/currency-converter",
-    //     data: "Rest Api"
+  {
+    id: 3,
+    categories: "websites",
+    name: "Edushare",
+    img: "./images/edushare.png",
+    description: "An Educational landing page for a tuition-free online public school ",
+    languages: ["Javascript", "ReactJS",],
+    link: "https://myedushare.netlify.app/",
+    github: "https://github.com/kayodeolowo/edushare-Landing-page"
 
-    // },
+  },
 
-   
 
-    
-     
+
+
+  {
+    id: 5,
+    categories: "websites",
+    name: "Tasty Foods",
+    img: "./images/Tasty.png",
+    description: "A Mobile Responsive Landing page for an Eatery  ",
+    languages: ["Javascript", "ReactJS",],
+    link: "https://tasty-foodies.netlify.app/",
+    github: "https://github.com/kayodeolowo/foodweb"
+
+  },
+
+  {
+    id: 6,
+    categories: "web apps",
+    name: "Budget Calculator",
+    img: "./images/budget.png",
+    description: "A website for totaling your daily expenditures. ",
+    languages: ["Javascript", "ReactJS",],
+    link: "https://mybudgets-calculator.netlify.app/",
+    github: "https://github.com/kayodeolowo/expenses_Calculator",
+    data: "JSon"
+
+  },
+
+
+
+  // {
+  //     id:8,
+  //      categories: "websites",
+  //     name: "Space Tourism",
+  //     type: "JSON",
+  //     img : "./images/spacetour.png",
+  //     description: "Space Tourism website with details about space Trips. ",
+  //      languages: ["Javascript","ReactJS", "Json" ],
+  //     link: "https://our-space-launch.netlify.app",
+  //     github: "https://github.com/kayodeolowo/Space-Tour",
+  //     data: "JSon"
+
+  // },
+
+  {
+    id: 9,
+    categories: "web apps",
+    name: "My Portfolio",
+    type: "Json",
+    img: "./images/portf.png",
+    description: "Hi Welcome to my portfolio. ",
+    languages: ["Javascript", "ReactJS", "Json"],
+    link: "https://kayodeolowo.netlify.app/",
+    github: "https://github.com/kayodeolowo/myportfolio",
+    data: "JSon"
+
+  },
+
+
+  // {
+  //    id:10,
+  //      categories: "websites",
+  //     name: "Eazy Bank",
+  //     img : "./images/eazybank.png",
+  //     description: "A responsive Banking landing page. ",
+  //      languages: ["Javascript", "ReactJS", ],
+  //     link: "https://my-eazy-bank.netlify.app/",
+  //     github: "https://github.com/kayodeolowo/easebank"
+
+  // },
+
+  // {
+  //    id:11,
+  //      categories: "web apps",
+  //     name: "Currency Converter",
+  //     type: "Rest API",
+  //     img : "./images/converter.png",
+  //    description: "Web App built with the functionality of Accurately converting currencies. ",
+  //     languages: ["Javascript", "ReactJS", "Api" ],
+
+  //     link: "https://my-currencies-converter.netlify.app/",
+  //     github: "https://github.com/kayodeolowo/currency-converter",
+  //     data: "Rest Api"
+
+  // },
+
+
+
+
+
 ]
 
 export default Categories
